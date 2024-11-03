fa3bd751d93bb4fbb0ddd345cb7395c438a2bffd
ISSEI-dot <g27e4e8bdeypv@gmail.com>
2024-11-03 14:29:18 +0900

    連想配列の作成

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>連想配列の作成</title>
+</head>
+<body>
+    <?php
+    $personal_data=['$name'=>'onion','$price'=>200,'$weight'=>160];
+    print_r($personal_data);
+    ?>
+</body>
+</html>
